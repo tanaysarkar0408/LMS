8dfda92f8e44ca2b717461090e2965b36701ba45
Tanay Sarkar <tanay35sarkar44@gmail.com>
2025-08-20 15:23:14 +0530

    update

--- client/src/Pages/educator/Dashboard.jsx
+++ client/src/Pages/educator/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../../Context/AppContext'
-import { assets } from '../../assets/assets.js'
+import { assets } from '../../../src/assets/assets.js'
 import Loading from '../../Components/Student/Loading'
 import axios from 'axios'
 import { toast } from 'react-toastify'
